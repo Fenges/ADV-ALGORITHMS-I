240d18d00b7db0e452ca2d0788761989128b85a1
Kratos <Kratos.X.Omega@gmail.com>
2018-09-09 03:03:40 -0400

    add c++ implementation of primeTest

---
+++ assignment_1/programming_project/src/header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+//#include <bits/stdc++.h>
+
+bool isPrime(int n, int k);
+
+#endif
--- assignment_1/programming_project/src/main.cpp
+++ assignment_1/programming_project/src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include "header.h"
+
 using namespace std;
 
-int main() 
+int main()
 {
-    cout << "Hello, World!" << endl;
-    return 0;
+  std::cout << "Miller-Rabin Primility Test: " << std::endl;
+  int k = 4;  // Number of iterations
+
+  for (int n = 1; n < 100; n++)
+    if (isPrime(n, k))
+      std::cout << n << " ";
+
+  return 0;
 }
